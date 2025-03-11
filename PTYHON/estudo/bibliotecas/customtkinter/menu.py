b24711e69922675f8d63f39f12aad49cad4df3f5
Micael Junco <192010202+9-f0xo@users.noreply.github.com>
2025-03-11 16:47:51 -0300

    menu.py

--- PTYHON/estudo/bibliotecas/customtkinter/menu.py
+++ PTYHON/estudo/bibliotecas/customtkinter/menu.py
@@ -5,38 +5,37 @@ from tkinter import messagebox
 class FrameDeTamanhos(ctk.CTkFrame):
     def __init__(self, app, titulo, valores):
         super().__init__(app)
+        self.app = app
         self.titulo = titulo
         self.valores = valores
         tamanhos = ['Broto', 'Pequena', 'Média', 'Grande', 'Avestruz']
-        self.tamanho = ctk.IntVar()
+        self.tamanho = ctk.IntVar(value=-1)
 
         self.titulo_frame = ctk.CTkLabel(self, text=self.titulo, fg_color="gray30", corner_radius=6)
         self.titulo_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 0), sticky="sew")
         
         for i, valor in enumerate(self.valores):
-            radiobutton = ctk.CTkRadioButton(self, text=f'{tamanhos[i]}: {(i+1)*4} fatias, até {valor} sabores', variable=self.tamanho, value=i, command=self.acessar_Tamanho)
-
+            radiobutton = ctk.CTkRadioButton(self, text=f'{tamanhos[i]}: {(i+1)*4} fatias, até {valor} sabores', 
+                variable=self.tamanho, value=i, command=self.acessar_Tamanho)
             radiobutton.grid(row=i+1, column=0, columnspan=2, padx=10, pady=5, sticky="sew")
 
     def acessar_Tamanho(self):
-        print('Funfando')
+        self.app.atualizarPreco_total()
 
     def confirmarEscolha(self):
-        if self.tamanho.get() == 0:
+        if self.tamanho.get() == -1:
             messagebox.showerror('ERRO', 'É obrigatório escolher algum tamanho de pizza.')
             return False
-        else: return True
+        return True
 
 
 class FrameDeSabores(ctk.CTkFrame):
     def __init__(self, app, titulo):
         super().__init__(app)
-        self.titulo = titulo
+        self.app = app
         self.vars = {}
-        self.totalVal = ctk.DoubleVar(value=0.0)  # Inicializa o total como 0
-        self.qtd_sabores_escolhidos = 0
-        
-        self.titulo_frame = ctk.CTkLabel(self, text=self.titulo, fg_color="gray30", corner_radius=6)
+
+        self.titulo_frame = ctk.CTkLabel(self, text=titulo, fg_color="gray30", corner_radius=6)
         self.titulo_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="sew")
         
         self.sabores = {
@@ -47,56 +46,60 @@ class FrameDeSabores(ctk.CTkFrame):
         for i, (sabor, valor) in enumerate(self.sabores.items(), start=1):
             self.vars[sabor] = ctk.IntVar(value=0)
             ctk.CTkCheckBox(self, text=f"{sabor} - R${valor}", variable=self.vars[sabor],
-                onvalue=1, offvalue=0, command=self.acessar_valDosSabores).grid(row=i, column=0, padx=10, pady=5, sticky="we")
+                            onvalue=1, offvalue=0, command=self.acessar_valDosSabores).grid(row=i, column=0, padx=10, pady=5, sticky="we")
             
     def acessar_valDosSabores(self):
-        # Obtém o tamanho escolhido
-        tamanho_escolhido = app.frame_de_tamanhos.tamanho.get()
+        tamanho_escolhido = self.app.frame_de_tamanhos.tamanho.get()
         if tamanho_escolhido < 0:
             return  # Nenhum tamanho foi selecionado ainda
 
-        limite_sabores = app.frame_de_tamanhos.valores[tamanho_escolhido]  # Obtém o limite de sabores
-        self.qtd_sabores_escolhidos = sum(var.get() for var in self.vars.values())
+        limite_sabores = self.app.frame_de_tamanhos.valores[tamanho_escolhido]
+        qtd_sabores = sum(var.get() for var in self.vars.values())
 
-        if self.qtd_sabores_escolhidos > limite_sabores:
+        if qtd_sabores > limite_sabores:
             messagebox.showwarning("Limite atingido", f"Você pode escolher no máximo {limite_sabores} sabores.")
             for sabor, var in self.vars.items():
                 if var.get() == 1:
-                    var.set(0)  # Desmarca o último selecionado'
+                    var.set(0)  # Desmarca o último selecionado
+        
+        self.app.atualizarPreco_total()
+
 
 class FrameDeAdicionais(ctk.CTkFrame):
     def __init__(self, app, titulo):
         super().__init__(app)
+        self.app = app
         
-        self.titulo = titulo
-        self.titulo_frame = ctk.CTkLabel(self, text=self.titulo, fg_color="gray30", corner_radius=6)
+        self.titulo_frame = ctk.CTkLabel(self, text=titulo, fg_color="gray30", corner_radius=6)
         self.titulo_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 0), sticky="sew")
 
-        self.quantidade = [str(i) for i in range(11)]
-        self.comboboxes = {}
         self.adicionais = {
             'Coca-Cola 2L': 10, 'Pepsi 2L': 9, 'Sprite 2L': 9, 'Fanta Laranja 2L': 8,
             'Coca-Cola 1,5L': 9, 'Pepsi 1,5L': 7.50, 'Sprite 1,5L': 7.50, 'Fanta Laranja 1,5L': 6.70,
         }
         
+        self.comboboxes = {}
+        
         for i, (adicional, valor) in enumerate(self.adicionais.items(), start=1):
             ctk.CTkLabel(self, text=f"{adicional} - R${valor}").grid(row=i, column=0, padx=10, pady=5, sticky="we")
-            combobox = ctk.CTkComboBox(self, values=self.quantidade, state="readonly", command=self.acessar_valDosAdicionais)
+            combobox = ctk.CTkComboBox(self, values=[str(i) for i in range(11)], state="readonly",
+                command=self.acessar_valDosAdicionais)
             combobox.set("0")
             combobox.grid(row=i, column=1, padx=10, pady=5, sticky='we')
             self.comboboxes[adicional] = combobox
 
-    def acessar_valDosAdicionais(self):
-        print('Funfando')
+    def acessar_valDosAdicionais(self, event=None):
+        '''O event=None é um parâmetro opcional da função acessar_valDosAdicionais. Ele permite que a função seja chamada tanto manualmente quanto automaticamente por eventos da interface gráfica (como mudanças em um ComboBox).'''
+        self.app.atualizarPreco_total()
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-
         self.title('Menu')
-        self.geometry('800x550')
-        self.grid_columnconfigure((0, 1, 2, 3), weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.geometry('820x580')
+        self.total_label = ctk.CTkLabel(self, text='Total: R$0.00', font=("Arial", 14))
+        self.total_label.grid(row=4, column=0, columnspan=4, padx=10, pady=5, sticky="sew")
 
         label1 = ctk.CTkLabel(self, text='Escolha como deseja que seu pedido seja realizado!', font=("Arial", 16))
         label2 = ctk.CTkLabel(self, text='Após escolher o tamanho, sabores e adicionais, clique no botão abaixo para confirmar seu pedido.', font=("Arial", 14))
@@ -106,7 +109,7 @@ class App(ctk.CTk):
         
         self.frame_de_tamanhos = FrameDeTamanhos(self, valores=[2, 3, 4, 5, 6], titulo='Escolha o tamanho da pizza:')
         self.frame_de_tamanhos.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
-
+        
         self.frame_de_sabores = FrameDeSabores(self, titulo='Escolha seus sabores')
         self.frame_de_sabores.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
         
@@ -117,17 +120,14 @@ class App(ctk.CTk):
         self.botao_confirmar.grid(row=3, column=0, columnspan=4, padx=10, pady=10, sticky="sew")
 
     def atualizarPreco_total(self):
-        preco_total = 0
-
-        label_total = ctk.CTkLabel(self, text=f'Total: R${preco_total:.2f}', font=("Arial", 14))
-        label_total.grid(row=1, column=2, columnspan=4, padx=10, pady=5, sticky="sew")
+        total = sum(valor for sabor, valor in self.frame_de_sabores.sabores.items() if self.frame_de_sabores.vars[sabor].get())
+        total += sum(int(self.frame_de_adicionais.comboboxes[adicional].get()) * valor for adicional, valor in self.frame_de_adicionais.adicionais.items())
+        self.total_label.configure(text=f'Total: R${total:.2f}')
     
     def confirmar_escolhas(self):
         if not self.frame_de_tamanhos.confirmarEscolha():
             return
-        else:
-            self.destroy()
-
+        self.destroy()
 
 ctk.set_appearance_mode('dark')
 app = App()
